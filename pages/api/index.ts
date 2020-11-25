69c567884c04cd58ad32ec99b15651fdd434bd16
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-11-25 21:20:19 +0200

    Feature: install next-connect

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,5 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  return res.send({ date: new Date() });
-};
+import nc from "next-connect";
+
+const handler = nc()
+  .get((_req: NextApiRequest, res: NextApiResponse) => {
+    return res.send({ date: new Date() });
+  })
+  .post((_req: NextApiRequest, res: NextApiResponse) => {
+    return res.send({ message: "It is POST method" });
+  });
+
+export default handler;
