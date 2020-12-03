381cbb4ccf8840cdfa6dd8235758b28954a5bdf4
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-12-03 19:40:21 +0200

    Feature: create ui for specific article page

--- pages/readlist/[slug].tsx
+++ pages/readlist/[slug].tsx
@@ -1,9 +1,33 @@
 /** @jsxRuntime classic */
 /** @jsx jsx  */
 
+import { GetServerSidePropsContext } from "next";
+import DefaultErrorPage from "next/error";
+import { FC } from "react";
+import ReactHtmlParser from "react-html-parser";
 import { Box, Heading, Text, Link, jsx } from "theme-ui";
 
-const Article = () => {
+import { CardProps } from "../../src/components/card/Card";
+import { transform } from "../../src/helpers/transform";
+
+interface ArticleProps extends CardProps {
+  text: string;
+  original: string;
+  error?: boolean;
+}
+
+const Article: FC<ArticleProps> = ({
+  title,
+  source,
+  ttr,
+  original,
+  text,
+  error,
+}) => {
+  if (error) {
+    return <DefaultErrorPage statusCode={404} />;
+  }
+
   return (
     <Box
       sx={{
@@ -16,73 +40,34 @@ const Article = () => {
         pb: 50,
       }}
     >
-      <Heading sx={{ variant: "styles.h1" }}>
-        Расшифровки аббревиатур (IMHO, AFAIK, LOL и т.п.)
-      </Heading>
+      <Heading sx={{ variant: "styles.h1" }}>{title}</Heading>
       <Box mt={10} mb={10}>
         <Text sx={{ variant: "styles.p", color: "muted" }}>
-          medium.com • 4 min
+          {source} • {ttr} min
         </Text>
         <Link
-          href="http://medium.com"
+          href={original}
           target="__blank"
           sx={{ variant: "styles.a", textDecoration: "none" }}
         >
           View original
         </Link>
+        <div>{ReactHtmlParser(text, { transform })}</div>
       </Box>
-      <Text variant="styles.p">
-        After publishing my previous article tracking React Libraries worth
-        talking about, a few people asked me how I decide which library to use
-        in my own projects.
-      </Text>
-      <Text variant="styles.p">
-        Truth is, I don’t have a single hard and fast rule, but rather a feeling
-        that comes from looking at {""}
-        <span sx={{ variant: "styles.p", fontWeight: "bold" }}>
-          how a library stacks up against a set of quick checks that. {""}
-        </span>
-        I’ve developed over the years of being burned by dodgy libraries.
-      </Text>
-      <Heading variant="styles.h2">Docs</Heading>
-      <Text variant="styles.p">
-        One of the first things I check before installing a library is the
-        documentation
-      </Text>
-      <Text variant="styles.p">
-        I’ve also been asked this several times over the years as an interview
-        question for frontend developer roles. I’ve heard different answers
-        every time I asked the question back to the interviewer - keep in mind
-        that some teams care about different things, and you might have to
-        decide for yourself.
-      </Text>
-      <Heading variant="styles.h2">Issues</Heading>
-      <Text variant="styles.p">
-        After publishing my previous article tracking React Libraries worth
-        talking about, a few people asked me how I decide which library to use
-        in my own projects.
-      </Text>
-      <Text variant="styles.p">
-        Truth is, I don’t have a single hard and fast rule, but rather a feeling
-        that comes from looking at how a library stacks up against a set of
-        quick checks that I’ve developed over the years of being burned by dodgy
-        libraries.
-      </Text>
-      <Text variant="styles.p">
-        I’ve also been asked this several times over the years as an interview
-        question for frontend developer roles. I’ve heard different answers
-        every time I asked the question back to the interviewer - keep in mind
-        that some teams care about different things, and you might have to
-        decide for yourself.
-      </Text>
-      <Heading variant="styles.h2">Conclusion</Heading>
-      <Text variant="styles.p">
-        After publishing my previous article tracking React Libraries worth
-        talking about, a few people asked me how I decide which library to use
-        in my own projects.
-      </Text>
     </Box>
   );
+};
+
+export const getServerSideProps = async ({
+  params,
+}: GetServerSidePropsContext) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API}/api/readlist/${params?.slug}`
+  );
+
+  const { data } = await response.json();
+
+  return { props: data };
 };
 
 export default Article;
