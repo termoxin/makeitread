43354c3fab595af7833a38a004a529c7702273e0
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-12-25 19:13:17 +0200

    Feature: do not show how many articles user has saved when he's not authenticated

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,42 @@
 /** @jsx jsx  */
 
 import Link from "next/link";
-import { Badge, Heading, jsx } from "theme-ui";
-import { GetServerSidePropsContext } from "next";
+import { Badge, Heading, jsx, Message } from "theme-ui";
 
 import { CardProps } from "@components/card/Card";
 import { fetchReadList } from "src/api/readlist";
+import { PageProps } from "./_app";
+import { FC } from "react";
+import { useSession } from "next-auth/client";
+import { NextPageContext } from "next";
 
-interface HomeProps {
+interface HomeProps extends PageProps {
   list: CardProps[];
 }
 
-const Home = ({ list }: HomeProps) => (
-  <div>
-    <Heading variant="styles.h2">
-      <Link href="/readlist">
-        <a sx={{ variant: "styles.a", cursor: "pointer" }}>Read List</a>
-      </Link>
-      <Badge ml={1}>{list.length}</Badge>
-    </Heading>
-  </div>
-);
+const Home: FC<HomeProps> = ({ list }) => {
+  const [session] = useSession();
 
-export const getServerSideProps = async () => {
-  const list = await fetchReadList();
+  return (
+    <div>
+      {session ? (
+        <Heading variant="styles.h2">
+          <Link href="/readlist">
+            <a sx={{ variant: "styles.a", cursor: "pointer" }}>Read List</a>
+          </Link>
+          <Badge ml={1}>{list.length}</Badge>
+        </Heading>
+      ) : (
+        <Message variant="primary" mt={50}>
+          Please, sing in to see your reading list
+        </Message>
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async (context: NextPageContext) => {
+  const list = await fetchReadList(context);
 
   return { props: { list } };
 };
