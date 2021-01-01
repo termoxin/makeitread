9400a90d350d6e69a7171dba6ee2961c80f8ad93
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-01-02 01:52:22 +0200

    Refactor: move deleting an article to another route

--- pages/api/readlist/[slug].ts
+++ pages/api/readlist/[slug].ts
@@ -4,34 +4,25 @@ import nc from "next-connect";
 import { Article } from "src/db/schemas";
 
 const getHandler = async ({ query }: NextApiRequest, res: NextApiResponse) => {
-  const slug = query.slug;
+  const { slug } = query;
 
-  const article = await Article.findOne({ slug });
+  const article = await Article.findOne({
+    slug,
+  });
 
-  if (article) {
-    const data = {
-      data: article._doc,
-    };
-
-    res.send(data);
-  } else {
-    res.send({
+  if (!article) {
+    return res.send({
       data: { error: true, message: `"${slug}" page doesn't found` },
     });
   }
+
+  const data = {
+    data: article,
+  };
+
+  return res.send(data);
 };
 
-const deleteHandler = async (
-  { query: { slug } }: NextApiRequest,
-  res: NextApiResponse
-) => {
-  const article = await Article.findOne({ slug });
-
-  await article.delete();
-
-  return res.send({ success: true });
-};
-
-const handler = nc().get(getHandler).delete(deleteHandler);
+const handler = nc().get(getHandler);
 
 export default handler;
---
+++ pages/api/readlist/[slug]/[email].ts
@@ -0,0 +1,54 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import nc from "next-connect";
+
+import { Article } from "src/db/schemas";
+
+const getHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, slug } = req.query;
+
+  const articleInMyReadlist = await Article.findOne({
+    slug,
+  }).where("email", email);
+
+  if (articleInMyReadlist) {
+  }
+
+  if (!articleInMyReadlist) {
+    const article = await Article.findOne({
+      slug,
+    });
+
+    if (!article) {
+      return res.send({
+        data: { error: true, message: `"${slug}" page doesn't found` },
+      });
+    }
+
+    const data = {
+      data: article,
+    };
+
+    return res.send(data);
+  } else {
+    const data = {
+      data: articleInMyReadlist,
+    };
+
+    return res.send(data);
+  }
+};
+
+const deleteHandler = async (
+  { query: { slug, email } }: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const article = await Article.findOne({ slug, email });
+
+  await article.delete();
+
+  return res.send({ success: true });
+};
+
+const handler = nc().get(getHandler).delete(deleteHandler);
+
+export default handler;
