1a1f11177564cc817c9e61ede3576a6bc8f7bdfc
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-01-02 02:00:29 +0200

    Refactor: save timestamps for each document

--- src/db/schemas.ts
+++ src/db/schemas.ts
@@ -7,22 +7,27 @@ const initializeModel = (modelName: modelNames, schema: Schema) =>
     ? mongoose.connection.models[modelName]
     : mongoose.model(modelName, schema);
 
-const articleSchema = new mongoose.Schema({
-  cover: String,
-  title: String,
-  description: String,
-  slug: String,
-  text: String,
-  original: String,
-  ttr: Number,
-  source: String,
-  marked: Boolean,
-});
+const articleSchema = new mongoose.Schema(
+  {
+    cover: String,
+    title: String,
+    description: String,
+    slug: String,
+    text: String,
+    original: String,
+    ttr: Number,
+    source: String,
+    marked: Boolean,
+    email: String,
+  },
+  { timestamps: true }
+);
 
 const noteSchema = new mongoose.Schema({
   text: String,
   url: String,
-});
+  email: String,
+}, {timestamps: true});
 
 export const Article = initializeModel("Article", articleSchema);
 export const Note = initializeModel("Note", noteSchema);
