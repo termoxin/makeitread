f77e0a6d201cf53e5bcb127aa079da0fd548b0d8
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-01-03 17:04:42 +0200

    Feature: add read article s visualizer

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,55 @@
 /** @jsx jsx  */
 
 import Link from "next/link";
-import { Badge, Heading, jsx, Message } from "theme-ui";
-import { FC } from "react";
+import { Badge, Box, Heading, jsx, Message } from "theme-ui";
+import { FC, useMemo } from "react";
 import { useSession } from "next-auth/client";
 import { NextPageContext } from "next";
 
 import { CardProps } from "@components/card/Card";
 import { fetchReadList } from "src/api/readlist";
 import { PageProps } from "./_app";
+import { CalendarHeatmap, ChartShallowDataShape } from "reaviz";
+import { getDayOfYear } from "src/helpers/client/date";
 
+interface ExpandedCard extends CardProps {
+  createdAt: Date;
+  updatedAt: Date;
+  markedAt: Date;
+}
 interface HomeProps extends PageProps {
-  list: CardProps[];
+  list: ExpandedCard[];
 }
 
 const Home: FC<HomeProps> = ({ list }) => {
   const [session] = useSession();
+
+  const heatmapData = useMemo(
+    () =>
+      Object.values(
+        list
+          .filter((article) => article.markedAt)
+          .map((article) => new Date(article.markedAt))
+          .reduce(
+            (acc: Record<number, ChartShallowDataShape>, currentValue) => {
+              const date = new Date(currentValue);
+              const dayOfYear = getDayOfYear(date);
+              const day = acc[dayOfYear];
+
+              return {
+                ...acc,
+                [dayOfYear]: {
+                  ...day,
+                  key: day?.key ?? date,
+                  data: day?.data ? +day.data + 1 : 1,
+                },
+              };
+            },
+            {}
+          )
+      ),
+    []
+  );
 
   return (
     <div>
@@ -31,6 +65,16 @@ const Home: FC<HomeProps> = ({ list }) => {
         <Message variant="primary" mt={50}>
           Please, sing in to see your reading list
         </Message>
+      )}
+      {!!heatmapData.length && (
+        <Box mt={20} ml={10}>
+          <CalendarHeatmap
+            sx={{ overflow: "visible" }}
+            data={heatmapData}
+            height={135}
+            width={805}
+          />
+        </Box>
       )}
     </div>
   );
