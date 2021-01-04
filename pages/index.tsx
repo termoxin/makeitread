4581f544094edf8594601f4785d34083dfb5e3e5
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-01-04 08:33:51 +0200

    Refactor: show calendar heatmap only when a user is authenticated and there are marked as read articles

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,43 +52,47 @@ const Home: FC<HomeProps> = ({ list }) => {
   return (
     <div>
       {session ? (
-        <Heading variant="styles.h2">
-          <Link href="/readlist">
-            <a sx={{ variant: "styles.a", cursor: "pointer" }}>Read List</a>
-          </Link>
-          <Badge ml={1}>{list.length}</Badge>
-        </Heading>
+        <Box>
+          <Heading variant="styles.h2">
+            <Link href="/readlist">
+              <a sx={{ variant: "styles.a", cursor: "pointer" }}>Read List</a>
+            </Link>
+            <Badge ml={1}>{list.length}</Badge>
+          </Heading>
+          {!!heatmapData.length && (
+            <Box mt={20} ml={10} sx={{ width: 700, height: 300 }}>
+              <ResponsiveCalendar
+                data={heatmapData}
+                from="2021-01-01"
+                to="2021-12-31"
+                emptyColor="#eeeeee"
+                colors={["#61cdbb", "#97e3d5", "#e8c1a0", "#f47560"]}
+                margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+                yearSpacing={40}
+                monthBorderColor="#ffffff"
+                dayBorderWidth={2}
+                dayBorderColor="#ffffff"
+                legends={[
+                  {
+                    anchor: "bottom-right",
+                    direction: "row",
+                    translateY: 36,
+                    itemCount: 4,
+                    itemWidth: 42,
+                    itemHeight: 36,
+                    itemsSpacing: 14,
+                    itemDirection: "right-to-left",
+                  },
+                ]}
+              />
+            </Box>
+          )}
+        </Box>
       ) : (
         <Message variant="primary" mt={50}>
           Please, sing in to see your reading list
         </Message>
       )}
-      <Box mt={20} ml={10} sx={{ width: 700, height: 300 }}>
-        <ResponsiveCalendar
-          data={heatmapData}
-          from="2021-01-01"
-          to="2021-12-31"
-          emptyColor="#eeeeee"
-          colors={["#61cdbb", "#97e3d5", "#e8c1a0", "#f47560"]}
-          margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
-          yearSpacing={40}
-          monthBorderColor="#ffffff"
-          dayBorderWidth={2}
-          dayBorderColor="#ffffff"
-          legends={[
-            {
-              anchor: "bottom-right",
-              direction: "row",
-              translateY: 36,
-              itemCount: 4,
-              itemWidth: 42,
-              itemHeight: 36,
-              itemsSpacing: 14,
-              itemDirection: "right-to-left",
-            },
-          ]}
-        />
-      </Box>
     </div>
   );
 };
